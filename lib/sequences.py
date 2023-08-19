319fd02e4efc9921f2f105f5a499ec8c65bc6a7a
ibrahimmohamed3 <ibra39167271@gmail.com>
2023-08-20 00:38:05 +0300

    complete

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,16 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
 def print_fibonacci(length):
-    pass
+    if length <= 0:
+        return []
+        
+
+    fibonacci_sequence = [0, 1]
+    while len(fibonacci_sequence) < length:
+        next_num = fibonacci_sequence[-1] + fibonacci_sequence[-2]
+        fibonacci_sequence.append(next_num)
+
+    print(fibonacci_sequence)
+
+
+print_fibonacci(9)  
